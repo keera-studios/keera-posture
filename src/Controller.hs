1916295ff3f90c90aa7630d2246e72216149a163
Ivan Perez <ivan.perez@keera.es>
2012-03-16 02:15:03 +0000

    Removes unused files. Uses cmdargs for command-line arguments.

--- src/Controller.hs
+++ src/Controller.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 -- | This contains the main controller. Many operations will be
 -- implemented in the Controller.* subsystem. This module simply
 -- initialises program.
@@ -7,11 +8,10 @@ module Controller where
 import           Control.Monad
 import qualified Control.Exception as E
 import           Control.Exception.Extra
-import           Data.List
 import           Hails.Graphics.UI.Gtk.Helpers.MessageDialog
 import           Hails.I18N.Gettext
 import           Hails.I18N.Language
-import           System.Environment
+import           System.Console.CmdArgs
 import           System.IO
 
 -- Internal imports
@@ -25,54 +25,38 @@ import MyIO
 -- installing the hanlders for all the conditions
 -- and starting the view.
 startController :: IO ()
-startController = handleAllExceptions reportSevereError $ do
+startController = do 
+   handleAllExceptions reportSevereError $ do
+     myPutStrLn "Starting Keera Posture"
+     installLanguage "keera-posture"
+   ags <- cmdArgs defArgs
+   handleAllExceptions reportSevereError $ startControllerWithArgs ags
 
-  myPutStrLn "Starting Keera Posture"
+startControllerWithArgs :: Args -> IO()
+startControllerWithArgs ags
+ | licence ags
+ = do canStdErr <- hIsOpen stderr
+      canStdOut <- hIsOpen stdout
+      let can = canStdErr || canStdOut
+          hdl = if canStdErr then stderr else stdout
+      when can $ mapM_ (hPutStrLn hdl)
+        [ "Keera Posture v0.0.1"
+        , "Copyright (C) 2010-2011 Ivan Perez Dominguez"
+        , __ "Licence: All rights reserved"
+        , __ "This software includes modified and unmodified versions of"
+        , __ "free and open source software. Read the copyright licence,"
+        , __ "the online documentation or contact support@keera.es to"
+        , __ "know which libraries Keera Posture depends on and to obtain"
+        , __ "the modified code of those libraries when that right is"
+        , __ "granted to you by their licence."
+        , ""
+        , __ "Written by Ivan Perez Dominguez"
+        ]
+ | otherwise
+ = startKeeraMain
 
-  installLanguage "keera-posture"
-  
-  -- Parse command-line args
-  args <- parseArgs
-
-  startControllerWithArgs args
-
-startControllerWithArgs :: [ Arg ] -> IO()
-startControllerWithArgs [ IncorrectArg ] =
-  reportSeverelyError (__ "You invocated Keera Posture with the wrong arguments. Please, read the online documentation or contact support@keera.es.")
-
-startControllerWithArgs ( ShowHelp : _ ) = do
-  canStdErr <- hIsOpen stderr
-  canStdOut <- hIsOpen stdout
-  let can = canStdErr || canStdOut
-      hdl = if canStdErr then stderr else stdout
-  when can $ do
-    hPutStrLn hdl $ __ "USAGE: keera-posture [OPTION]"
-    hPutStrLn hdl $ __ "Track your sitting position and warn you when you move"
-    hPutStrLn hdl $ ""
-    hPutStrLn hdl $ __ "  -h, --help     print this message"
-    hPutStrLn hdl $ __ "  -v, --version  print the version info and exit"
-    hPutStrLn hdl $ ""
-    hPutStrLn hdl $ __ "Report keera-posture bugs to support@keera.es"
-
-startControllerWithArgs ( ShowVersion: _ ) = do
-  canStdErr <- hIsOpen stderr
-  canStdOut <- hIsOpen stdout
-  let can = canStdErr || canStdOut
-      hdl = if canStdErr then stderr else stdout
-  when can $ do
-    hPutStrLn hdl $ "Keera Posture v0.0.1"
-    hPutStrLn hdl $ "Copyright (C) 2010-2011 Ivan Perez Dominguez"
-    hPutStrLn hdl $ __ "Licence: All rights reserved"
-    hPutStrLn hdl $ __ "This software includes modified and unmodified versions of"
-    hPutStrLn hdl $ __ "free and open source software. Read the copyright licence,"
-    hPutStrLn hdl $ __ "the online documentation or contact support@keera.es to"
-    hPutStrLn hdl $ __ "know which libraries Keera Posture depends on and to obtain"
-    hPutStrLn hdl $ __ "the modified code of those libraries when that right is"
-    hPutStrLn hdl $ __ "granted to you by their licence."
-    hPutStrLn hdl   ""
-    hPutStrLn hdl $ __ "Written by Ivan Perez Dominguez"
-
-startControllerWithArgs _ = do
+startKeeraMain :: IO ()
+startKeeraMain = do
 
   -- Initialise the visual layer
   initView
@@ -90,8 +74,8 @@ startControllerWithArgs _ = do
   startView
 
 reportSevereError :: IO ()
-reportSevereError = reportSeverelyError s
- where s = __ "Keera Posture crashed. Please, contact support@keera.es."
+reportSevereError = reportSeverelyError $
+ __ "Keera Posture crashed. Please, contact support@keera.es."
 
 reportSeverelyError :: String -> IO()
 reportSeverelyError s = do
@@ -102,19 +86,19 @@ reportSeverelyError s = do
     startView
  where t = __ "Keera Posture"
 
-parseArgs :: IO [ Arg ]
-parseArgs = do
-  args <- getArgs
-  return $ sort $ map toArg args
+data Args = Args
+   { licence :: Bool }
+ deriving (Show, Data, Typeable)
 
-toArg :: String -> Arg
-toArg "-h"        = ShowHelp
-toArg "--help"    = ShowHelp
-toArg "-v"        = ShowVersion
-toArg "--version" = ShowVersion
-toArg _           = IncorrectArg
-
-data Arg = IncorrectArg
-         | ShowHelp
-         | ShowVersion
- deriving (Eq, Ord)
+-- This is the cmdArgs-based CLI interface definition
+defArgs :: Args
+defArgs = Args
+          { licence = def
+          &= help "Shows the licence and exits"
+          }
+         &= program "keera-posture"
+         &= summary "Keera Posture 0.0.1 (c) 2010-2012 Ivan Perez - Keera Studios"
+         &= details [ __ "Report bugs to ivan.perez@keera.es"
+                    , __ "Find more about Keera Posture at http://keera.es"
+                    , __ "and http://github.com/ivanperez-keera/keera-posture"
+                    ]
