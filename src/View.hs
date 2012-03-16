e887cd576170e3971da2f59cfce442677a41955b
Ivan Perez <ivan.perez@keera.es>
2012-03-16 00:23:39 +0000

    Uses code in hails instead of replicating it

--- src/View.hs
+++ src/View.hs
@@ -1,36 +1,17 @@
 -- | Contains basic operations related to the GUI
-module View where
+module View
+  (module View
+  , module Exported)
+  where
 
 -- External libraries
 import Graphics.UI.Gtk
--- import Graphics.UI.Gtk.GenericView (GUI(..))
 import Graphics.UI.Gtk.GtkView
--- import Hails.MVC.View.GtkView
+import Hails.MVC.View.GtkView as Exported
 
 -- Internal libraries
 import View.MainWindow.Objects
 import View.MainWindow.InitLanguagesCombo
-
--- | Initialises the GUI. This must be called before
--- any other GUI operation.
-initView :: IO ()
-initView = initGUI >>= \_ -> return ()
-
--- | Starts a thread for the view.
-startView :: IO ()
-startView = mainGUI
-
--- | Executes an operation on the view thread synchronously
-onViewSync :: IO a -> IO a
-onViewSync = postGUISync
-
--- | Executes an operation on the view thread asynchronously
-onViewAsync :: IO () -> IO ()
-onViewAsync = postGUIAsync
-
--- | Destroys the view thread
-destroyView :: IO ()
-destroyView = mainQuit
 
 instance GtkGUI View where
   initialise = createView
@@ -41,7 +22,7 @@ data View = View
   { mainWindowBuilder :: Builder
   , languageListStore :: LanguageListStore
   -- , camerasListStore  :: CameraListStore
-  , statusIcon :: StatusIcon
+  -- , statusIcon :: StatusIcon
   }
 
 createView :: IO View
@@ -50,13 +31,13 @@ createView = do
 
   langs <- initLanguagesCombo bldr
   -- cams  <- initCamerasCombo bldr
-  icon <- trayIcon bldr
+  -- icon <- trayIcon bldr
 
   return
     View
       { mainWindowBuilder = bldr
       , languageListStore = langs
       -- , camerasListStore  = cams
-      , statusIcon        = icon
+      -- , statusIcon        = icon
       }
 
