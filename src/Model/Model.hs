2c77f3b7a91b6d49c7c125d3ed6b8bd4289c8682
Ivan Perez Dominguez <ivan.perez@keera.es>
2013-06-20 09:35:06 +0200

    Updates version numbers and contact into

--- src/Model/Model.hs
+++ src/Model/Model.hs
@@ -151,7 +151,7 @@ emptyBM = Model
  
   -- Update check
   , programName         = "Keera Posture"
-  , programVersion      = Version 0 0 Beta 0
+  , programVersion      = Version 0 1 Beta 0
   , programMaxVersion   = Nothing
   , programUpdateURI    = "http://www.keera.es/projects/keera-posture/newest-version"
  
