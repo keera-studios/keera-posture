786cfaa1b4770caf0c17bf64335f849c23115f29
Ivan Perez Dominguez <ivan.perez@keera.es>
2013-06-20 09:30:31 +0200

    Temptative installation instructions

---
+++ installation.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+git clone --depth=1 git://github.com/keera-studios/gtk-helpers.git
+git clone --depth=1 git://github.com/keera-studios/hails-i18n.git
+git clone --depth=1 git://github.com/keera-studios/hails-mvc-controller.git
+git clone --depth=1 git://github.com/keera-studios/hails-mvc-environment-gtk.git
+git clone --depth=1 git://github.com/keera-studios/hails-mvc-model-protectedmodels.git
+git clone --depth=1 git://github.com/keera-studios/hails-mvc-solutions-config.git
+git clone --depth=1 git://github.com/keera-studios/hails-mvc-solutions-gtk.git
+git clone --depth=1 git://github.com/keera-studios/hails-mvc-view.git
+git clone --depth=1 git://github.com/keera-studios/hails-mvc-view-gtk.git
+git clone --depth=1 git://github.com/keera-studios/hails-reactive-gtk.git
+git clone --depth=1 git://github.com/keera-studios/hails-reactivevalues.git
+git clone --depth=1 git://github.com/keera-studios/keera-hails.git
+git clone --depth=1 git://github.com/keera-studios/keera-posture.git
+git clone --depth=1 git://github.com/keera-studios/MissingK.git
+git clone --depth=1 git://github.com/sinelaw/HOpenCV.git
+
+export PATH=$PATH:`pwd`/cabal-dev/bin
+cabal-dev add-source gtk-helpers
+cabal-dev add-source hails-i18n
+cabal-dev add-source hails-mvc-controller
+cabal-dev add-source hails-mvc-environment-gtk
+cabal-dev add-source hails-mvc-model-protectedmodels
+cabal-dev add-source hails-mvc-solutions-config
+cabal-dev add-source hails-mvc-solutions-gtk
+cabal-dev add-source hails-mvc-view
+cabal-dev add-source hails-mvc-view-gtk
+cabal-dev add-source hails-reactive-gtk
+cabal-dev add-source hails-reactivevalues
+cabal-dev add-source keera-hails
+cabal-dev add-source keera-posture
+cabal-dev add-source MissingK
+cabal-dev add-source HOpenCV
+
+cabal-dev install keera-posture
