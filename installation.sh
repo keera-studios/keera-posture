d95095ac5021c1fbc34d7596fafbbc609ad23f4e
Ivan Perez Dominguez <ivan.perez@keera.es>
2014-04-02 21:06:03 +0200

    Checks out specific version of HOpenCV. Installs gtk2hs and keera-hails only if not installed.

--- installation.sh
+++ installation.sh
@@ -23,7 +23,12 @@ git clone --depth=1 git://github.com/keera-studios/hails-reactivevalues.git
 git clone --depth=1 git://github.com/keera-studios/keera-hails.git
 git clone --depth=1 git://github.com/keera-studios/keera-posture.git
 git clone --depth=1 git://github.com/keera-studios/MissingK.git
-git clone --depth=1 git://github.com/sinelaw/HOpenCV.git
+
+# OpenCV 2.0 is not backwards compatible
+git clone git://github.com/sinelaw/HOpenCV.git
+pushd HOpenCV
+git checkout 99e3ffd # Revert to 0.1.2.4
+popd
 
 # Why cabal-dev does not do this automatically, I don't know
 export PATH=$PATH:`pwd`/cabal-dev/bin
@@ -69,9 +74,30 @@ cabal-dev add-source MissingK
 cabal-dev add-source HOpenCV
 
 # Install more dependencies
-cabal-dev install gtk2hs-buildtools
-cabal-dev install keera-hails
+which gtk2hsC2hs
+if [[ "$?" -gt "0" ]] ; then
+   cabal-dev install gtk2hs-buildtools;
+fi
+
+which hails
+if [[ "$?" -gt "0" ]] ; then
+   cabal-dev install keera-hails;
+fi
 
 # Keera Posture
 cabal-dev add-source keera-posture
-cabal-dev install keera-posture
+if [[ ! -z "$DEBIAN_BUILD" ]]; then
+  PACKAGE_NAME=keera-posture
+  DEBIAN_LIB_DIR=/usr/lib/$PACKAGE_NAME
+  DEBIAN_BIN_DIR=/usr/bin
+  DEBIAN_DATA_DIR=/usr/share/$PACKAGE_NAME/data/
+  # Prepare debian build
+  pushd keera-posture
+  cabal-dev -s ../cabal-dev/ install-deps
+  cabal-dev -s ../cabal-dev/ configure --libdir=$DEBIAN_LIB_DIR --bindir=$DEBIAN_BIN_DIR --datadir=$DEBIAN_DATA_DIR
+  cabal-dev -s ../cabal-dev/ build
+  popd
+else
+  # Normal installation
+  cabal-dev install keera-posture
+fi
