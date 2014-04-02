456784f459820a4974c9a8e0bdef10b511cf8651
Ivan Perez Dominguez <ivan.perez@keera.es>
2014-04-02 23:55:23 +0200

    Makes the script a bit more silent. Installs program before recompiling for distribution

--- installation.sh
+++ installation.sh
@@ -9,26 +9,42 @@
 # out for a spin
 
 # Check out all necessary repositories
-git clone --depth=1 git://github.com/keera-studios/gtk-helpers.git
-git clone --depth=1 git://github.com/keera-studios/hails-i18n.git
-git clone --depth=1 git://github.com/keera-studios/hails-mvc-controller.git
-git clone --depth=1 git://github.com/keera-studios/hails-mvc-environment-gtk.git
-git clone --depth=1 git://github.com/keera-studios/hails-mvc-model-protectedmodels.git
-git clone --depth=1 git://github.com/keera-studios/hails-mvc-solutions-config.git
-git clone --depth=1 git://github.com/keera-studios/hails-mvc-solutions-gtk.git
-git clone --depth=1 git://github.com/keera-studios/hails-mvc-view.git
-git clone --depth=1 git://github.com/keera-studios/hails-mvc-view-gtk.git
-git clone --depth=1 git://github.com/keera-studios/hails-reactive-gtk.git
-git clone --depth=1 git://github.com/keera-studios/hails-reactivevalues.git
-git clone --depth=1 git://github.com/keera-studios/keera-hails.git
-git clone --depth=1 git://github.com/keera-studios/keera-posture.git
-git clone --depth=1 git://github.com/keera-studios/MissingK.git
+echo -n Cloning
+git clone --quiet --depth=1 git://github.com/keera-studios/gtk-helpers.git
+echo -n .
+git clone --quiet --depth=1 git://github.com/keera-studios/hails-i18n.git
+echo -n .
+git clone --quiet --depth=1 git://github.com/keera-studios/hails-mvc-controller.git
+echo -n .
+git clone --quiet --depth=1 git://github.com/keera-studios/hails-mvc-environment-gtk.git
+echo -n .
+git clone --quiet --depth=1 git://github.com/keera-studios/hails-mvc-model-protectedmodels.git
+echo -n .
+git clone --quiet --depth=1 git://github.com/keera-studios/hails-mvc-solutions-config.git
+echo -n .
+git clone --quiet --depth=1 git://github.com/keera-studios/hails-mvc-solutions-gtk.git
+echo -n .
+git clone --quiet --depth=1 git://github.com/keera-studios/hails-mvc-view.git
+echo -n .
+git clone --quiet --depth=1 git://github.com/keera-studios/hails-mvc-view-gtk.git
+echo -n .
+git clone --quiet --depth=1 git://github.com/keera-studios/hails-reactive-gtk.git
+echo -n .
+git clone --quiet --depth=1 git://github.com/keera-studios/hails-reactivevalues.git
+echo -n .
+git clone --quiet --depth=1 git://github.com/keera-studios/keera-hails.git
+echo -n .
+git clone --quiet --depth=1 git://github.com/keera-studios/keera-posture.git
+echo -n .
+git clone --quiet --depth=1 git://github.com/keera-studios/MissingK.git
+echo -n .
 
 # OpenCV 2.0 is not backwards compatible
-git clone git://github.com/sinelaw/HOpenCV.git
-pushd HOpenCV
-git checkout 99e3ffd # Revert to 0.1.2.4
-popd
+git clone --quiet git://github.com/sinelaw/HOpenCV.git
+pushd HOpenCV > /dev/null
+git checkout --quiet 99e3ffd # Revert to 0.1.2.4
+popd > /dev/null
+echo [DONE]
 
 # Why cabal-dev does not do this automatically, I don't know
 export PATH=$PATH:`pwd`/cabal-dev/bin
@@ -86,18 +102,22 @@ fi
 
 # Keera Posture
 cabal-dev add-source keera-posture
+
+# Normal installation (even if we want to build a DEB, we install
+# so that we can later copy only the data files we really need
+# (FIXME: this is a bit ugly)
+cabal-dev install keera-posture
+
 if [[ ! -z "$DEBIAN_BUILD" ]]; then
   PACKAGE_NAME=keera-posture
   DEBIAN_LIB_DIR=/usr/lib/$PACKAGE_NAME
   DEBIAN_BIN_DIR=/usr/bin
-  DEBIAN_DATA_DIR=/usr/share/$PACKAGE_NAME/data/
+  DEBIAN_DATA_DIR=/usr/share/$PACKAGE_NAME/
+  DEBIAN_DATA_SUBDIR=""
   # Prepare debian build
   pushd keera-posture
   cabal-dev -s ../cabal-dev/ install-deps
-  cabal-dev -s ../cabal-dev/ configure --libdir=$DEBIAN_LIB_DIR --bindir=$DEBIAN_BIN_DIR --datadir=$DEBIAN_DATA_DIR
+  cabal-dev -s ../cabal-dev/ configure --libdir=$DEBIAN_LIB_DIR --bindir=$DEBIAN_BIN_DIR --datadir=$DEBIAN_DATA_DIR --datasubdir=$DEBIAN_DATA_SUBDIR
   cabal-dev -s ../cabal-dev/ build
   popd
-else
-  # Normal installation
-  cabal-dev install keera-posture
 fi
