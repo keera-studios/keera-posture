e6502fc9b221d02b02eca89885dd7af610b6d0db
Ivan Perez Dominguez <ivan.perez@keera.co.uk>
2014-04-06 16:06:04 +0200

    Adjusts permissions of installation script before running it

--- debian/package-from-scratch.sh
+++ debian/package-from-scratch.sh
@@ -2,5 +2,6 @@
 PACKAGE=keera-posture
 REPO=git@bitbucket.org:iperezdominguez/$PACKAGE-prerelease.git
 git archive --remote=$REPO HEAD installation.sh | tar -x > installation.sh
+chmod a+x installation.sh
 DEBIAN_BUILD=1 ./installation.sh
 $PACKAGE/debian/create-package.sh
