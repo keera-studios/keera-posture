7fe4c420899a3c5afc60202c582f099a3e45d371
Ivan Perez Dominguez <ivan.perez@keera.co.uk>
2014-04-06 16:44:03 +0200

    Refresh sudo if necessary

--- debian/package-from-scratch.sh
+++ debian/package-from-scratch.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+sudo -v -n true
+if [[ "$?" -ge "1" ]]; then
+  echo Cannot sudo without password. Execute sudo first, then run this command.
+  exit 1
+fi
 PACKAGE=keera-posture
 REPO=git@bitbucket.org:iperezdominguez/$PACKAGE-prerelease.git
 git archive --remote=$REPO HEAD installation.sh | tar -x > installation.sh
 chmod a+x installation.sh
 DEBIAN_BUILD=1 ./installation.sh
+sudo -v # Refresh sudo
 $PACKAGE/debian/create-package.sh
