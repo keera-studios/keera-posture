fce7e4af8f367b7b99c5d813081c6f983c6e335b
Ivan Perez Dominguez <ivan.perez@keera.co.uk>
2014-04-06 17:23:42 +0200

    Installs necessary debian deps

--- debian/package-from-scratch.sh
+++ debian/package-from-scratch.sh
@@ -6,8 +6,12 @@ if [[ "$?" -ge "1" ]]; then
 fi
 PACKAGE=keera-posture
 REPO=git@bitbucket.org:iperezdominguez/$PACKAGE-prerelease.git
+echo [1] Installing debian dependencies
+git archive --remote=$REPO HEAD:debian debian-deps.sh | tar -x -O | bash
+echo [2] Extracting and compiling all packages
 git archive --remote=$REPO HEAD installation.sh | tar -x > installation.sh
 chmod a+x installation.sh
 DEBIAN_BUILD=1 ./installation.sh
 sudo -v # Refresh sudo
+echo [3] Building debian package
 $PACKAGE/debian/create-package.sh
