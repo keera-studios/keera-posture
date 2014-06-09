04d26d76d6e5ceaf42913c9076890524aab60e6e
Ivan Perez <ivan.perez@keera.co.uk>
2014-06-09 18:03:55 +0200

    Uses https instead of git

--- debian/package-from-scratch.sh
+++ debian/package-from-scratch.sh
@@ -5,11 +5,13 @@ if [[ "$?" -ge "1" ]]; then
   exit 1
 fi
 PACKAGE=keera-posture
-REPO=git@bitbucket.org:iperezdominguez/$PACKAGE-prerelease.git
+REPO=https://bitbucket.org/iperezdominguez/$PACKAGE-prerelease/raw/master
 echo [1] Installing debian dependencies
-git archive --remote=$REPO HEAD:debian debian-deps.sh | tar -x -O | bash
+wget $REPO/debian/debian-deps.sh  
+chmod a+x debian-deps.sh
+./debian-deps.sh 
 echo [2] Extracting and compiling all packages
-git archive --remote=$REPO HEAD installation.sh | tar -x > installation.sh
+wget $REPO/installation.sh
 chmod a+x installation.sh
 DEBIAN_BUILD=1 ./installation.sh
 sudo -v # Refresh sudo
